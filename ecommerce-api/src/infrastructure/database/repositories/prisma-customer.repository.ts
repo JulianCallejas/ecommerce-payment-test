1f1d1d87a41813a98d2a1ff76773157e45e21ecf
JulianCallejas <juliecallejas@hotmail.com>
2025-05-24 13:19:53 -0500

    infrastructure-customer repository

---
+++ ecommerce-api/src/infrastructure/database/repositories/prisma-customer.repository.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { CustomerRepositoryPort } from 'src/core/ports/repositories/customer.repository.port';
+import { PrismaService } from '../prisma.service';
+import { Customer } from 'src/core/entities/customer.entity';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class PrismaCustomerRepository implements CustomerRepositoryPort {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: string): Promise<Customer | null> {
+    return this.prisma.customer.findUnique({
+      where: { id },
+    });
+  }
+
+  async findByCustomerId(customerId: string): Promise<Customer | null> {
+    return this.prisma.customer.findUnique({
+      where: { customerId },
+    });
+  }
+
+  async findAll(page: number, pageSize: number): Promise<[Customer[], number]> {
+    const skip = (page - 1) * pageSize;
+    
+    const [customers, count] = await Promise.all([
+      this.prisma.customer.findMany({
+        skip,
+        take: pageSize,
+        orderBy: { createdAt: 'desc' },
+      }),
+      this.prisma.customer.count(),
+    ]);
+    
+    return [customers, count];
+  }
+
+  async create(customer: Partial<Customer>): Promise<Customer> {
+    return this.prisma.customer.create({
+      data: customer as Prisma.CustomerCreateInput,
+    });
+  }
+
+  async findCustomerWithOrders(id: string): Promise<Customer | null> {
+    return this.prisma.customer.findUnique({
+      where: { id },
+      //TODO - include orders
+    });
+  }
+}
