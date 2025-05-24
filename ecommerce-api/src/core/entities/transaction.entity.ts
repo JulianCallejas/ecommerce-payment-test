92ca3a4a80fa5d400858e66d98f12d7dbc336958
JulianCallejas <juliecallejas@hotmail.com>
2025-05-24 16:57:41 -0500

    Prisma-transaction schema, Core-transaction entity

---
+++ ecommerce-api/src/core/entities/transaction.entity.ts
@@ -0,0 +1,22 @@
+import { Decimal } from '@prisma/client/runtime/library';
+import { Order } from './order.entity';
+
+export enum TransactionStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  DECLINED = 'DECLINED',
+  VOIDED = 'VOIDED',
+  ERROR = 'ERROR',
+}
+
+export class Transaction {
+  id: string;
+  externalId: string;
+  orderId: string;
+  order?: Order;
+  status: TransactionStatus;
+  amount: Decimal;
+  details: any;  //Accepts JSON from external provider
+  createdAt: Date;
+  updatedAt: Date;
+}
