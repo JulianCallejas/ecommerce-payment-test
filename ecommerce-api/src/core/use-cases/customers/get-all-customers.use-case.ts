f8d4e48fb6e855af1845a1e8c58ea562e8bdcbb3
JulianCallejas <juliecallejas@hotmail.com>
2025-05-24 13:26:24 -0500

    core-customer use case update

--- ecommerce-api/src/core/use-cases/customers/get-all-customers.use-case.ts
+++ ecommerce-api/src/core/use-cases/customers/get-all-customers.use-case.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CustomerRepositoryPort } from '../../ports/repositories/customer.repository.port';
 
 @Injectable()
 export class GetAllCustomersUseCase {
-  constructor(private readonly customerRepository: CustomerRepositoryPort) {}
+  constructor(
+    @Inject('CustomerRepositoryPort')
+    private readonly customerRepository: CustomerRepositoryPort
+  ) {}
 
   async execute(page: number, pageSize: number) {
     return this.customerRepository.findAll(page, pageSize);
--- ecommerce-api/src/core/use-cases/customers/get-customer-with-orders.use-case.ts
+++ ecommerce-api/src/core/use-cases/customers/get-customer-with-orders.use-case.ts
@@ -1,16 +1,20 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CustomerRepositoryPort } from '../../ports/repositories/customer.repository.port';
 
 @Injectable()
 export class GetCustomerWithOrdersUseCase {
-  constructor(private readonly customerRepository: CustomerRepositoryPort) {}
+  constructor(
+    @Inject('CustomerRepositoryPort')
+    private readonly customerRepository: CustomerRepositoryPort
+  ) {}
 
   async execute(customerId: string) {
-    const customer = await this.customerRepository.findCustomerWithOrders(customerId);
+    const customer =
+      await this.customerRepository.findCustomerWithOrders(customerId);
     if (!customer) {
       throw new NotFoundException(`Customer with id ${customerId} not found`);
     }
-    
+
     return customer;
   }
 }
