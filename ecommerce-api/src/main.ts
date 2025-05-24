fa14eb81cb13c007a9f2b2eaeec43b0eef52ce53
JulianCallejas <juliecallejas@hotmail.com>
2025-05-24 00:59:58 -0500

    settings-helmet, cors, prefix and swagger configuration

--- ecommerce-api/src/main.ts
+++ ecommerce-api/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.use(helmet());
+  app.enableCors();
+
+   app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce API')
+    .setDescription('E-Commerce API')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
